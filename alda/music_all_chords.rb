d41fa847b119c3d118c8edd32aa1a8c6dbedc259
Bill Westfall <billwestfall@yahoo.com>
2016-03-11 15:23:56 -0800

    Create music_all_chords.rb

---
+++ alda/music_all_chords.rb
@@ -0,0 +1,34 @@
+
+#!/usr/bin/ruby
+
+print "piano: (tempo! 20) "
+  $i = 0
+    while $i < 640 do
+      print [ :o2, :o3, :o4, :o5, :o6, :o7, :<, :> ].sample
+      print " "
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :-, :+, :"", :"" ].sample
+      print [ :"4", :"8", :"16", :"32", :"" ].sample
+      print "/"
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :-, :+, :"", :"" ].sample
+      print [ :"4", :"8", :"16", :"32", :"" ].sample
+      print "/"
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :-, :+, :"", :"" ].sample
+      print [ :"4", :"8", :"16", :"32", :"" ].sample
+      print "/"
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :-, :+, :"", :"" ].sample
+      print [ :"4", :"8", :"16", :"32", :"" ].sample
+      print "/"
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :-, :+, :"", :"" ].sample
+      print [ :"4", :"8", :"16", :"32", :"" ].sample
+      print "/"
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :-, :+, :"", :"" ].sample
+      print [ :"4", :"8", :"16", :"32", :"" ].sample
+      print " "
+      $i +=1
+    end
