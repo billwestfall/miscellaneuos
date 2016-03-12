6b3d2fd08f94b4d333f9d6a2bfc58d5cbaa283a9
Bill Westfall <billwestfall@yahoo.com>
2016-03-12 13:02:31 -0800

    Create music_song_slow.rb
    ruby file to create the alda music

---
+++ feldmanizer/music_song_slow.rb
@@ -0,0 +1,28 @@
+
+#!/usr/bin/ruby
+
+print "piano: (tempo! 40) "
+  $i = 0
+    while $i < 640 do
+      print [ :o6, :o7, :o8, :o9, :o10, :<, :> ].sample
+      print " "
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :"2", :"4", :"" ].sample
+      print [ :/, :"" ].sample
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :"2", :"4", :"" ].sample
+      print [ :/, :"" ].sample
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :"2", :"4", :"" ].sample
+      print [ :/, :"" ].sample
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :"2", :"4", :"" ].sample
+      print [ :/, :"" ].sample
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :"2", :"4", :"" ].sample
+      print [ :/, :"" ].sample
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :"2", :"4", :"" ].sample
+      print " "
+      $i +=1
+    end
