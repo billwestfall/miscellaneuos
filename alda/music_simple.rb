f2bd5db0506c14b5a06880979ddec20a428dea1a
billw@concur.com <billw@concur.com>
2017-03-01 14:58:21 -0800

    Tweak the simple music params

--- alda/music_simple.rb
+++ alda/music_simple.rb
@@ -4,12 +4,12 @@
 print "piano: (tempo! 40) "
   $i = 0
     while $i < 640 do
-      print [ :o4, :o5, :o6, :o7, :o8, :<, :> ].sample
+      print [ :o3, :o4, :o5, :o6, :o7, :<, :> ].sample
       print " "
       print [ :a, :b, :c, :d, :e, :f, :g, ].sample
       print [ :-, :+, :"", :"" ].sample
       print [ :"4", :"8", :"16", :"32", :"" ].sample
-#      print [ :/, :"" ].sample
+#      print [ :/, :" " ].sample
 #      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
 #      print [ :-, :+, :"", :"" ].sample
 #      print [ :"4", :"8", :"16", :"32", :"" ].sample
@@ -26,12 +26,12 @@ print "piano: (tempo! 40) "
     print "midi-honky-tonk-piano: (tempo! 40) "
       $i = 0
         while $i < 640 do
-          print [ :o4, :o5, :o6, :o7, :o8, :<, :> ].sample
+          print [ :o3, :o4, :o5, :o6, :o7, :<, :> ].sample
           print " "
           print [ :a, :b, :c, :d, :e, :f, :g, ].sample
           print [ :-, :+, :"", :"" ].sample
           print [ :"4", :"8", :"16", :"32", :"" ].sample
-    #      print [ :/, :"" ].sample
+    #      print [ :/, :" " ].sample
     #      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
     #      print [ :-, :+, :"", :"" ].sample
     #      print [ :"4", :"8", :"16", :"32", :"" ].sample
@@ -42,3 +42,25 @@ print "piano: (tempo! 40) "
           print " "
           $i +=1
         end
+
+        puts ""
+        puts ""
+        print "midi-bright-acoustic-piano: (tempo! 40) "
+          $i = 0
+            while $i < 640 do
+              print [ :o3, :o4, :o5, :o6, :o7, :<, :> ].sample
+              print " "
+              print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+              print [ :-, :+, :"", :"" ].sample
+              print [ :"4", :"8", :"16", :"32", :"" ].sample
+        #      print [ :/, :" " ].sample
+        #      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+        #      print [ :-, :+, :"", :"" ].sample
+        #      print [ :"4", :"8", :"16", :"32", :"" ].sample
+        #      print [ :/, :"" ].sample
+        #      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+        #      print [ :-, :+, :"", :"" ].sample
+        #      print [ :"4", :"8", :"16", :"32", :"" ].sample
+              print " "
+              $i +=1
+            end
