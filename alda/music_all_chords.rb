5d1745ae0a05b739757fa10b57165d9f51485dc2
billw@concur.com <billw@concur.com>
2017-03-02 14:33:32 -0800

    Update to 240 reps

--- alda/music_all_chords.rb
+++ alda/music_all_chords.rb
@@ -1,9 +1,9 @@
 
 #!/usr/bin/ruby
 
-print "piano: (tempo! 5) "
+print "piano: (tempo! 60) "
   $i = 0
-    while $i < 160 do
+    while $i < 240 do
       print [ :o2, :o3, :o4, :o5, :o6, :o7, :<, :> ].sample
       print " "
       print [ :a, :b, :c, :d, :e, :f, :g, ].sample
