843c2eb9161f8046bddfc0a3fec3a73a01125642
billw@concur.com <billw@concur.com>
2017-03-16 20:45:08 -0700

    Print random pattern for Noatikl

--- noatikl/patterns.rb
+++ noatikl/patterns.rb
@@ -1,1 +1,18 @@
 
+#!/usr/bin/ruby
+
+      print "<"
+  $i = 0
+    while $i < 10 do
+      print rand( 10..100 )
+      print " "
+      print [ :"B", :"F" ].sample
+      print " "
+      print rand( 10..240 )
+      print " "
+      print rand( 1..15 )
+      print " "
+      $i +=1
+    end
+          print ">"
+          puts ""
