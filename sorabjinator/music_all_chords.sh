885b424fe2704fe85f993b00a90603b2ff718b16
Bill Westfall <billwestfall@yahoo.com>
2017-05-02 19:29:11 -0700

    Update music_all_chords.sh
    move to all chords

--- sorabjinator/music_all_chords.sh
+++ sorabjinator/music_all_chords.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/ruby
 
-print "piano: (tempo! 40) "
+print "piano: (tempo! 60) "
   $i = 0
     while $i < 160 do
-      print [ :o6, :o7, :o8, :o9, :o10, :<, :> ].sample
+      print [ :o3, :o4, :o5, :o6, :o7, :<, :> ].sample
       print " "
       print [ :a, :b, :c, :d, :e, :f, :g, ].sample
       print [ :"2", :"4", :"" ].sample
-      print [ :/, :" " ].sample
+      print "/"
       print [ :a, :b, :c, :d, :e, :f, :g, ].sample
       print [ :"2", :"4", :"" ].sample
-      print [ :/, :" " ].sample
+      print "/"
       print [ :a, :b, :c, :d, :e, :f, :g, ].sample
       print [ :"2", :"4", :"" ].sample
-      print [ :/, :" " ].sample
+      print "/"
       print [ :a, :b, :c, :d, :e, :f, :g, ].sample
       print [ :"2", :"4", :"" ].sample
-      print [ :/, :" " ].sample
+      print "/"
       print [ :a, :b, :c, :d, :e, :f, :g, ].sample
       print [ :"2", :"4", :"" ].sample
-      print [ :/, :" " ].sample
+      print "/"
       print [ :a, :b, :c, :d, :e, :f, :g, ].sample
       print [ :"2", :"4", :"" ].sample
       print " "
