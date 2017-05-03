4900bbe26a6351aacb5c3cd32401493d6ac07bdc
Bill Westfall <billwestfall@yahoo.com>
2017-05-02 19:46:50 -0700

    Update music_all_chords.rb
    remove the seventh octave sounds too brittle

--- sorabjinator/music_all_chords.rb
+++ sorabjinator/music_all_chords.rb
@@ -3,7 +3,7 @@
 print "piano: (tempo! 60) "
   $i = 0
     while $i < 160 do
-      print [ :o3, :o4, :o5, :o6, :o7, :<, :> ].sample
+      print [ :o3, :o4, :o5, :o6, :<, :> ].sample
       print " "
       print [ :a, :b, :c, :d, :e, :f, :g, ].sample
       print [ :"2", :"4", :"" ].sample
