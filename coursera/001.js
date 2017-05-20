c9a261681537733c2bbd18add5092ad9ec5c8f06
billw@concur.com <billw@concur.com>
2017-05-20 09:26:46 -0700

    Move coursera repo

---
+++ coursera/001.js
@@ -0,0 +1,13 @@
+/* MacBook-Air:cs101 billw$ /System/Library/Frameworks/JavaScriptCore.framework/Versions/A/Resources/jsc -f 001.js
+42
+1 2 47
+Alice Alice Alice Alice
+In high school I had a crush on Alice
+Now the Alice curse is lifted */
+
+print (42);
+print (1, 2, 47);
+x = "Alice";
+print(x, x, x, x);
+print("In high school I had a crush on", x);
+print("Now the", x , "curse is lifted");
