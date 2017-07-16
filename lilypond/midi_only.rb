2f104c63060b3f83f361fb78a261d7c30ba4e93d
billw@concur.com <billw@concur.com>
2017-07-16 11:38:49 -0700

    First overtone project

---
+++ lilypond/midi_only.rb
@@ -0,0 +1,132 @@
+#!/usr/bin/ruby
+
+print "\\version \"2.19.22\""
+print "\n"
+print "\\include \"english.ly\""
+print "\n"
+print "\\header {"
+print "\n"
+print "title = \\markup { \"Overture Colossale\" }"
+print "\n"
+print "composer = \"Bill Westfall\""
+print "\n"
+print "}"
+print "\n"
+
+print "\\paper {"
+print "\n"
+print "paper-height = 4.6\\in"
+print "\n"
+print "paper-width = 8.5\\in"
+print "\n"
+print "indent = #0"
+print "\n"
+print " system-count = #2"
+print "\n"
+print "}"
+print "\n"
+
+print "\\score {"
+print "\n"
+print "\\new PianoStaff = \"pianostaff\" <<"
+print "\n"
+print "\\new Staff = \"RH\" \\relative c\' {"
+print "\n"
+print "     \\set midiInstrument = #\"piano\""
+print "\n"
+print "     \\accidentalStyle Score.piano"
+print "\n"
+print "     \\clef \"treble\""
+print "\n"
+print "      \\time 4\/4"
+print "\n"
+print "       \\key "
+key = [ :a, :b, :c, :d, :e, :f, :g, ].sample
+print key
+print "        \\"
+signature = [ :major, :minor ].sample
+print signature
+      print "  "
+print "\n"
+print "      \\set Score.currentBarNumber = #1"
+print "\n"
+print "     \\tempo \"sequitur\" 4 = 30"
+print "\n"
+print "  "
+  $i = 0
+    while $i < 1000 do
+      print "<"
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [:s, :f, ""].sample
+      print [ :"'", :",", :"''", :",,", :"" ].sample
+      print " "
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [:s, :f, ""].sample
+      print [ :"'", :",", :"''", :",,", :"" ].sample
+      print " "
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [:s, :f, ""].sample
+      print [ :"'", :",", :"''", :",,", :"" ].sample
+      print " "
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [:s, :f, ""].sample
+      print [ :"'", :",", :"''", :",,", :"" ].sample
+      print " "
+      print ">"
+      print [ :"4 ", :"8", :"16", :"32", :"64", :"128" ].sample
+#      print " "
+#      print "\\bar \"\" "
+      $i +=1
+    end
+print "\n"
+print "}"
+print "\n"
+
+print "    \\new Staff = \"LH\" \\relative c, {"
+print "     \\set midiInstrument = #\"piano\""
+print "     \\clef \"bass\""
+print "      \\time 4\/4"
+print "       \\key "
+print key
+print "        \\"
+print signature
+      print "  "
+        $i = 0
+          while $i < 1000 do
+            print "<"
+            print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+            print [:s, :f, ""].sample
+            print [ :"'", :",", :"''", :",,", :"" ].sample
+            print " "
+            print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+            print [:s, :f, ""].sample
+            print [ :"'", :",", :"''", :",,", :"" ].sample
+            print " "
+            print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+            print [:s, :f, ""].sample
+            print [ :"'", :",", :"''", :",,", :"" ].sample
+            print " "
+            print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+            print [:s, :f, ""].sample
+            print [ :"'", :",", :"''", :",,", :"" ].sample
+            print " "
+            print ">"
+            print [ :"4 ", :"8", :"16", :"32", :"64", :"128" ].sample
+      #      print " "
+      #      print "\\bar \"\" "
+            $i +=1
+          end
+      print "\n"
+      print "}"
+      print "\n"
+  print ">>"
+  print "\n"
+#  print "\\layout { }"
+#  print "\n"
+  print "\\midi {"
+  print "\n"
+  print "\n"
+  print "}"
+  print "\n"
+  print "}"
+  print "\n"
