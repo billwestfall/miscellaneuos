0f4413e510ff88715093a45681bfcdd7a0b46bd5
Bill Westfall <billwestfall@yahoo.com>
2018-02-12 16:08:17 -0800

    Update 003.js

--- javascript/javascripting/chapt01/003.js
+++ javascript/javascripting/chapt01/003.js
@@ -1,6 +1,8 @@
+<script src="update.js" type="text/javascript">
 function unsplat(fun) { return function() {
 return fun.call(null, _.toArray(arguments)); };
 }
 var joinElements = unsplat(function(array) { return array.join(' ') });
 joinElements(1, 2);
 joinElements('-', '$', '/', '!', ':');
+</script>
