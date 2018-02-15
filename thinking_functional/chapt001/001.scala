ff160850ad9e9ba703104cd4ad338808defb1479
Bill Westfall <billwestfall@yahoo.com>
2018-02-15 14:20:01 -0800

    Create 001.scala

---
+++ thinking_functional/chapt001/001.scala
@@ -0,0 +1,10 @@
+scala> def indexOfAny(input : String, searchChars : Seq[Char]) : Seq[Int] = {
+     | def indexedInput = (0 until input.length).zip(input)
+     | for (pair <- indexedInput;
+     | char <- searchChars;
+     | if (char == pair._2)) yield (pair._1)
+     | }
+indexOfAny: (input: String, searchChars: Seq[Char])Seq[Int]
+
+scala> indexOfAny("zzabyycdxx", "by")
+res1: Seq[Int] = Vector(3, 4, 5)
