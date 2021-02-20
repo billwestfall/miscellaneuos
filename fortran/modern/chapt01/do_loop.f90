af7ed8de257a17bf65454108bd385d1fe8a81c3d
Bill Westfall <billwestfall@yahoo.com>
2021-02-20 13:35:05 -0800

    Update do_loop.f90

--- fortran/modern/chapt01/do_loop.f90
+++ fortran/modern/chapt01/do_loop.f90
@@ -1,15 +1,5 @@
-program do_loop
-
-  implicit none
-
-  real, parameter :: i=2
-  real, parameter :: j=3
-  real, parameter :: m=4
-
-  do j, j*m    
-    do i, i*m     
-      print *, 'i, j =', i, j      
-    end do          
-  end do
-
-end program do_loop
+outer_loop: doj=1,jm    
+  inner_loop: doi=1,im     
+    print *, 'i,j=',i,j      
+  end do inner_loop           
+end do outer_loop
