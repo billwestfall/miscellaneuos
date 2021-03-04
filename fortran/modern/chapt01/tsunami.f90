3698f9f75d9425b02b6c3408975f8deaeec5bb74
Bill Westfall <billwestfall@yahoo.com>
2021-03-04 13:44:08 -0800

    Update tsunami.f90

--- fortran/modern/chapt01/tsunami.f90
+++ fortran/modern/chapt01/tsunami.f90
@@ -1,9 +1,8 @@
-program tsunami     
+program tsunami    
 
   implicit none       
   
   integer :: i, n                                         
-  
   integer, parameter :: grid_size = 100                   
   integer, parameter :: num_time_steps = 100              
   
@@ -11,14 +10,35 @@ program tsunami
   real, parameter :: dx=1!gridspacing [m]            
   real, parameter ::c=1!background flow speed [m/s]  
   
-  real :: h(grid_size), dh(grid_size)                     
+  real :: h(grid_size), dh(grid_size)     
   
   integer, parameter :: icenter = 25                      
-  real, parameter :: decay = 0.02 
+  real, parameter :: decay = 0.02                         
   
   if (grid_size <= 0) stop 'grid_size must be > 0'          
   if (dt <= 0) stop 'time step dt must be > 0'              
   if (dx <= 0) stop 'grid spacing dx must be > 0'           
-  if (c <= 0) stop 'background flow speed c must be > 0'
-
+  if (c <= 0) stop 'background flow speed c must be > 0'    
+  
+  do concurrent(i = 1:grid_size)
+    h(i) = exp(-decay * (i - icenter)**2)   
+  end do 
+  
+  print *, 0, h     
+  
+  time_loop: don=1,num_time_steps    
+  
+    dh(1) = h(1) - h(grid_size)      
+    
+  do concurrent (i = 2:grid_size)
+    dh(i) = h(i) - h(i-1)       
+  end do
+  
+  do concurrent (i = 1:grid_size)
+    h(i)=h(i)-c*dh(i) / dx * dt     
+  end do
+  
+  print *, n, h    
+  
+end do time_loop
 end program tsunami
