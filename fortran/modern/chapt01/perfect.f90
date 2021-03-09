0ffca42764843b893f31e4a1f153db23dba89a6b
Bill Westfall <billwestfall@yahoo.com>
2021-03-09 15:05:41 -0800

    Update perfect.f90

--- fortran/modern/chapt01/perfect.f90
+++ fortran/modern/chapt01/perfect.f90
@@ -1,9 +1,9 @@
 program do_loop
 
   integer :: i = 0
-  INTEGER :: x(5) = (/ 1, 2, 3, 4 ,5 /)
-  print *, SUM(x)                        ! all elements, sum = 15
-  print *, SUM(x, MASK=MOD(x, 2)==1)     ! odd elements, sum = 9
+  integer :: x(5) = (/ 1, 2, 3, 4 ,5 /)
+  print *, sum(x)                        ! all elements, sum = 15
+  print *, sum(x, mask=mod(x, 2)==1)     ! odd elements, sum = 9
   
   do 
     i = i + 1
