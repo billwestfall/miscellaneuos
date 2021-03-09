244d7f80b474a9ea679c3cfb66f4db5a689935ba
Bill Westfall <billwestfall@yahoo.com>
2021-03-09 14:42:51 -0800

    Update do_loop.f90

--- fortran/modern/chapt01/do_loop.f90
+++ fortran/modern/chapt01/do_loop.f90
@@ -1,6 +1,11 @@
-INTEGER :: i = 0
-DO 
-  i = i + 1
-  WRITE(*, *) i
-  IF (MOD(i, 6) == 0) EXIT
-END DO
+program do_loop
+
+  INTEGER :: i = 0
+
+  DO 
+    i = i + 1
+    WRITE(*, *) i
+    IF (MOD(i, 6) == 0) EXIT
+  END DO
+
+end program do_loop
