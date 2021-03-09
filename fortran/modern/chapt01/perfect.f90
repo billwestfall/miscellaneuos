297c145a75ec40b398971303d4b81c13249e770b
Bill Westfall <billwestfall@yahoo.com>
2021-03-09 15:00:31 -0800

    Update perfect.f90

--- fortran/modern/chapt01/perfect.f90
+++ fortran/modern/chapt01/perfect.f90
@@ -1,11 +1,12 @@
 program do_loop
 
   integer :: i = 0
-  integer :: j = 1
-
+  INTEGER :: x(5) = (/ 1, 2, 3, 4 ,5 /)
+  print *, SUM(x)                        ! all elements, sum = 15
+  print *, SUM(x, MASK=MOD(x, 2)==1)     ! odd elements, sum = 9
+  
   do 
     i = i + 1
-    j = j + 1
     write(*, *) i
     if (mod(i, 6) == 0) EXIT
   end do
