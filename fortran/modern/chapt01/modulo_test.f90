f9ea071f85463b22e577f60ac96e0c5d950486dc
Bill Westfall <billwestfall@yahoo.com>
2021-03-09 13:52:26 -0800

    Create modulo_test.f90

---
+++ fortran/modern/chapt01/modulo_test.f90
@@ -0,0 +1,10 @@
+program test_modulo
+  print *, modulo(17,3)
+  print *, modulo(17.5,5.5)
+
+  print *, modulo(-17,3)
+  print *, modulo(-17.5,5.5)
+
+  print *, modulo(17,-3)
+  print *, modulo(17.5,-5.5)
+end program
