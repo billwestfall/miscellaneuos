34563cf324f68ab8fea6ebcd704d367b3f62ab72
Bill Westfall <billwestfall@yahoo.com>
2022-12-08 12:37:44 -0800

    Update 001_price.scala

--- scala/anth/001_price.scala
+++ scala/anth/001_price.scala
@@ -1,2 +1,6 @@
-val prices = List(211.10, 310.12, 510.45, 645.60, 832.33)
-prices.foreach { e => println(e) }
+object Hello {
+    def main(args: Array[String]) = {
+        val prices = List(211.10, 310.12, 510.45, 645.60, 832.33)
+        prices.foreach { e => println(e) }
+    }
+}
