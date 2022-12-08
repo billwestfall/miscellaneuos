3ee5d7c929b06551b08be748227f073009b31661
Bill Westfall <billwestfall@yahoo.com>
2022-12-08 12:04:00 -0800

    Update 001_oddeven.clj

--- clojure/anth/001_oddeven.clj
+++ clojure/anth/001_oddeven.clj
@@ -1,10 +1,10 @@
 (ns clojure.examples.odda
    (:gen-class))
 (defn odda []
-   print (filter odd? [0 1 2 3])
+   print (filter odd? [0 1 2 3]))
 (odda)
 (ns clojure.examples.evena
    (:gen-class))
 (defn evena []
-   print (filter even? [0 1 2 3])
+   print (filter even? [0 1 2 3]))
 (evena)
