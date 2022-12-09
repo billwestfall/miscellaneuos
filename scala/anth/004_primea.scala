09c304304a9a856d5b53433ce4d44707edf15982
Bill Westfall <billwestfall@yahoo.com>
2022-12-09 11:45:59 -0800

    Update 004_primea.scala

--- scala/anth/004_primea.scala
+++ scala/anth/004_primea.scala
@@ -3,3 +3,8 @@ def isPrime(number : Int) = {
   val hasFactorsOtherThan1AndItself =
     (2 to sqrtOfNumber).exists { i => number % i == 0 }
   number > 1 && !hasFactorsOtherThan1AndItself
+
+def primes(number : Int) : Stream[Int] = {
+  if(isPrime(number)) number #:: primes(number + 1) else primes(number + 1)
+}
+println(primes(1).take(10).toList
