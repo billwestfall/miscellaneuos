b7bd3add2d12605fce863e6812cd300d6a850152
Bill Westfall <billwestfall@yahoo.com>
2022-12-14 14:59:45 -0800

    Update 001_hello.clj

--- clojure/get/001_hello.clj
+++ clojure/get/001_hello.clj
@@ -1,7 +1,7 @@
 (ns clojure.examples.hello
    (:gen-class))
 (defn Example []
-   (println (str "Clo" "jure") ; Returns "Clojure".)
-   (println (str "Hello," " " "world" "!") ; Returns the string "Hello, world!")
-   (println  (str 3 " " 2 " " 1 " Blast off!") ; Fly me to the Moon!))
+   (println (str "Clo" "jure"))
+   (println (str "Hello," " " "world" "!"))
+   (println  (str 3 " " 2 " " 1 " Blast off!")))
 (Example)
