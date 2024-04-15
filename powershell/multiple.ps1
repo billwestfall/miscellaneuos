4a0224c9af504c4d1a40e95c09b82903c6642d6a
Bill Westfall <billwestfall@yahoo.com>
2024-04-15 12:21:18 -0700

    Create multiple.ps1

---
+++ powershell/multiple.ps1
@@ -0,0 +1,19 @@
+### Functions ###
+
+function function1 {
+    write-host "function 1 is executing"
+}
+
+function function2 {
+    write-host "function 2 is executing"
+}
+
+function function3 {
+    write-host "function 3 is executing"
+}
+
+### Start of process ###
+
+function3
+function2
+function1
