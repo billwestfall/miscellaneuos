e264852e57101aad8ac5319a215b210aab8999b7
Bill Westfall <billwestfall@yahoo.com>
2024-04-15 11:55:28 -0700

    Create TestDrive.Tests.ps1

---
+++ powershell/tests/TestDrive.Tests.ps1
@@ -0,0 +1,19 @@
+
+BeforeAll {
+    function Add-Footer($path, $footer) {
+        Add-Content $path -Value $footer
+    }
+}
+
+Describe "Add-Footer" {
+    BeforeAll {
+        $testPath = "TestDrive:\test.txt"
+        Set-Content $testPath -value "my test text."
+        Add-Footer $testPath "-Footer"
+        $result = Get-Content $testPath
+    }
+
+    It "adds a footer" {
+        (-join $result) | Should -Be "my test text.-Footer"
+    }
+}
