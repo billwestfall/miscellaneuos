c84001a8ecf188c04e87d6670f0f393b03f01af7
Bill Westfall <billwestfall@yahoo.com>
2024-04-16 15:49:51 -0700

    Update get_part.ps1

--- powershell/sys_admin/get_part.ps1
+++ powershell/sys_admin/get_part.ps1
@@ -1,6 +1,6 @@
 # Define function
 function View-Part {
-    $currentPart= Get-CimInstance -Class Win32_LogicalDisk | Select-Object -Property Name, FreeSpace
+    $currentPart= Get-Process | Get-Member | Out-Host -Property Name
     Write-Host "$currentPart"
 }
  
