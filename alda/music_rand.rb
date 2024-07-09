5757883231fd1acda024f431936ab6d0347aa71f
Bill Westfall <billwestfall@yahoo.com>
2024-07-09 11:47:36 -0700

    Update music_rand.rb

--- alda/music_rand.rb
+++ alda/music_rand.rb
@@ -1,15 +1,44 @@
-
 #!/usr/bin/ruby
 
-print "piano: "
+print "piano: (tempo 5) "
   $i = 0
-    while $i < 320 do
-      print [ :o0, :o1, :o2, :o3, :o4, :o5, :o6, :o7, :o8, :o9, :<, :> ].sample
+    while $i < 640 do
+      print [ :o0, :o1, :o2, :o3, :o4, :o5, :o6, :o7, :o8].sample
       print " "
       print [ :a, :b, :c, :d, :e, :f, :g, ].sample
-      print [ :-, :+, :"", :"" ].sample
-      print [ :"4", :"8", :"16", :"32", :"" ].sample
-#      print [ :/, :"" ].sample
+      print [ :"+", :"-", :"", :"" ].sample
+      print [ :"4", :"8", :"16", :"" ].sample
+      print [ :"/", :"" ].sample
+      print " "
+      $i +=1
+    end
+print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+puts ""
+puts ""
+print "piano: (tempo 5) "
+  $i = 0
+    while $i < 640 do
+      print [ :o0, :o1, :o2].sample
+      print " "
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :"+", :"-", :"", :"" ].sample
+      print [ :"4", :"8", :"" ].sample
+      print [ :"/", :"" ].sample
+      print " "
+      $i +=1
+    end
+print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+puts ""
+puts ""
+print "piano: (tempo 5) "
+  $i = 0
+    while $i < 640 do
+      print [ :o0, :o1, :o2, :o3, :o4, :o5].sample
+      print " "
+      print [ :a, :b, :c, :d, :e, :f, :g, ].sample
+      print [ :"+", :"-", :"", :"" ].sample
+      print [ :"4", :"8", :"16", :"" ].sample
+      print [ :"" ].sample
       print " "
       $i +=1
     end
